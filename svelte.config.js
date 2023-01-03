d5732fb2114051e1687f8db25fcac670c2bc34a3
Anil Ghale <anilghale11@gmail.com>
2023-01-03 10:58:56 +0545

    added adapter static

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 
@@ -10,4 +10,11 @@ const config = {
 	preprocess: vitePreprocess()
 };
 
-export default config;
+export default {
+	kit: {
+	  adapter: adapter({
+		fallback: '200.html'
+	  }),
+	  prerender: { entries: [] }
+	}
+  };
